a2ca5f7257426c363cebc9e504ab1de632fd9aa0
s-yeong <syxxng25@gmail.com>
2022-08-04 16:00:20 +0900

    스프링 핵심 원리 이해2 : 새로운 정책 개발

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,39 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    // 정말 10프로 할인이 되는지 보기
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    // 실패 테스트도 꼭 만들어 봐야함
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+
+
+
+}
