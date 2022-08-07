2bd40a8d60cfa988df6d4aab125ea7e5f8be5a3d
s-yeong <syxxng25@gmail.com>
2022-08-07 23:36:54 +0900

    스프링 핵심 원리 이해2 : 새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,8 +8,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository  memberRepository= new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+// DiscountPolicy에도 의존, FixDiscountPolicy도 의존 -> DIP 위반
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+// RateDiscountPolicy로 변경하는 순간, OrderServiceImpl 소스코드도 변경해야함 -> OCP 위반
+    private DiscountPolicy discountPolicy;
+    // 인터페이스에만 의존! - 추상화에만 의존
+    // 해결 방법: 누군가 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주어야한다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
