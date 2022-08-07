f8a21b6ae5b1a446682b286299a9b40c403400f7
s-yeong <syxxng25@gmail.com>
2022-08-08 08:18:50 +0900

    스프링 핵심 원리 이해2 - 스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,13 +1,16 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 이전에는 객체를 생성하고, 인터페이스에 어떤 것이 들어가는지 직접 했었음
@@ -16,22 +19,26 @@ public class AppConfig {
 
     // !- 역할을 드러나게 하는것이 중요함 - memberRepository, discountPolicy  (역할과 구현을 한눈에 들어오게)
     // 메서드 명을 보면 역할이 다 드러나게 했음 (현재 나의 애플리케이션에서는 memberService인데, memberServiceImpl을 쓸거야)
-    // 현재 나의 애플리케이션에선느 memberRepository에 대한 것은 memoryMemberRepository를 쓸거야
+    // 현재 나의 애플리케이션에선 memberRepository에 대한 것은 memoryMemberRepository를 쓸거야
     // OrderService를 쓰는데, 현재 내 애플리케이션에서 memberRepository 쓰는 거, discountPolicy쓰는 거를 가져올 거야
+    @Bean   // Spring 컨테이너에 등록!!
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     } // --> 생성자 주입
 
-    private MemoryMemberRepository memberRepository( ) {
+    @Bean
+    public MemoryMemberRepository memberRepository( ) {
         return new MemoryMemberRepository();    // jdbc로 바뀌면 "이 코드"만 바꾸면 된다!!
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 모든게 ApplicationContext로 시작.. 이게 Spring 컨테이너, 모든 것을 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig에 있는 환경설정 정보를 가지고 스프링 컨테어너에 안에 있는 객체 생성해서 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // 스프링 컨테이너에서 꺼내기
+
         // appconfig에서 memberService 인터페이스를 받음
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
