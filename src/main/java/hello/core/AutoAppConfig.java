6912b0e5a41d5381b20b5d996962bfe78339c856
s-yeong <syxxng25@gmail.com>
2022-08-21 21:05:36 +0900

    의존관계 자동 주입 - 옵션 처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -14,11 +14,13 @@ import org.springframework.context.annotation.FilterType;
         // 설정 정보는 컴포넌트 스캔 대상에서 제외(예제 코드 남기기 위해)
 )
 public class AutoAppConfig {
+/*
 
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+*/
 
 
 }
