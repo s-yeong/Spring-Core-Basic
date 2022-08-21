143fc2aa06f1601016748709208770b70b9adc8d
s-yeong <syxxng25@gmail.com>
2022-08-21 17:03:21 +0900

    컴포넌트 스캔 - 중복등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,6 +14,11 @@ import org.springframework.context.annotation.FilterType;
         // 설정 정보는 컴포넌트 스캔 대상에서 제외(예제 코드 남기기 위해)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 
 }
