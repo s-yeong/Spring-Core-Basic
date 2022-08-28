9862d959eaaa713407cd3129579be5baf04d7fc4
s-yeong <syxxng25@gmail.com>
2022-08-28 16:56:06 +0900

    빈 스코프 - 스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,13 +1,15 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.UUID;
 
-@Scope(value = "request")   // HTTP 요청 당 하나씩 생성, 끝나는 시점에서 소멸
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)   // HTTP 요청 당 하나씩 생성, 끝나는 시점에서 소멸
+// MyLogger의 가짜 프록시 객체 생성(요청이 오면 그때 내부에서 진짜 빈을 요청)
 @Component
 public class MyLogger {
 
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,7 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
     // MyLogger를 주입받는게 아니라, MyLogger를 찾을 수 있는 DL할 수 있는 애가 주입됨
 
 
@@ -22,9 +23,11 @@ public class LogDemoController {
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
 
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();  // 고객이 어떤 url로 요청했는지 알 수 있음
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
+        // 실제 기능을 호출하는 시점에 진짜 myLogger를 찾아서 넣어줌
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
