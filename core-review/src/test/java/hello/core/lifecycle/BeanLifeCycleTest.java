e92ab8e0b39f4b7dd932a9741eaf3ac9ad89b2ca
s-yeong <syxxng25@gmail.com>
2023-01-02 10:27:21 +0900

    (review) 빈 생명주기 콜백

---
+++ core-review/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,36 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void LifeCycleTest() {
+
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        // 스프링 컨테이너 종료
+        ac.close();
+    }
+
+
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://core.dev");
+            return networkClient;
+        }
+
+    }
+}
